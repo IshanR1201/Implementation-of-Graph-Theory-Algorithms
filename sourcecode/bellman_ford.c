481a5ab53dadd8a9adb605c594326e34616d2813
IshanR1201 <98479717+IshanR1201@users.noreply.github.com>
2022-01-26 11:34:32 -0600

    Add files via upload

---
+++ sourcecode/bellman_ford.c
@@ -0,0 +1,53 @@
+//Bellman-Ford algorithm for find shortest path
+
+#include<stdio.h>
+#include<stdlib.h>
+#define N 8					        //size of matrix
+#define INF 9999			      //weight of disconnected nodes
+
+int head[N][N]={			
+ {0,1,INF,INF,7,INF,INF,INF},       // matrix representing the weights of edges
+ {1,0,3,INF,INF,3,5,INF},
+ {INF,3,0,4,INF,INF,1,INF},
+ {INF,INF,4,0,INF,INF,2,3},
+ {7,INF,INF,INF,0,2,INF,INF},
+ {INF,3,INF,INF,2,0,INF,INF},
+ {INF,5,1,2,INF,INF,0,6},
+ {INF,INF,INF,3,INF,INF,6,0}};
+
+
+void bellman_ford();
+void print();
+
+
+int prev[N][N];                     //minimum cost os D1 ~ D6
+int current[N][N]; 
+
+int main()
+{
+	bellman_ford();
+	print();
+	return 0;
+}
+void bellman_ford()
+{
+	//Hint
+	// 1. define local variables as needed
+  // 2. initialize previous values
+  // 3. initialize the current values 
+  // in nested for loops
+  // 4. check if the distance to the node w is greater than (distance to v + distance b/w v and w)
+  // 5. if the condition above is true, update the current distance of w to the new value
+  // 6. set previous value to the current value for the next node
+}
+void print()
+{
+	int i=0;
+	printf("Bellman Ford Argorithm!!(start = node1)\n");
+	printf("========================================\n");
+	printf("     destination             cost \n");
+	for(i = 1; i < N; i++)                                          //print the distance to the destination
+	{
+        	printf("%11d%21d\n",i+1, current[i][N-2]);
+	}
+}
---
+++ sourcecode/dfs.c
@@ -0,0 +1,101 @@
+#include<stdio.h>
+
+#define WHITE 1
+#define GRAY 2
+#define BLACK 3
+#define NIL 100
+#define INFINITY 10000
+
+int dfs(int u);
+
+/* 'n' is the number of nodes and 'time' is the discovery time of a node */
+int n,time;
+
+/* Adjacency matrix to hold the graph */
+
+int **adj;
+
+/* Matrix to hold the graph's node color */
+
+int *color;
+
+/* Matrix to hold the predecessor node values */
+
+int *pred;
+
+/* Matrix to hold the discovery time of a node */
+
+int *dist;
+
+/* Matrix to hold the finishing time of a node */
+
+int *finish;
+
+int main()
+ {
+   int i,j;
+
+   printf("\n Enter the number of nodes: ");
+   scanf("%d",&n);
+
+   /* Memory allocation for all matrices is done here */
+
+   adj = (int**)malloc(sizeof(int*));
+
+   for(i=0;i<n;i++)
+	   adj[i] = (int*)malloc(n*sizeof(int));
+
+   color=(int*)malloc(n*sizeof(int));
+   pred=(int*)malloc(n*sizeof(int));
+   dist=(int*)malloc(n*sizeof(int));
+   finish=(int*)malloc(n*sizeof(int));
+
+   printf("\n Enter the adjacency matrix: ");
+
+   for(i=0;i<n;i++)
+    {
+     for(j=0;j<n;j++)
+       scanf("%d",&adj[i][j]);
+    }
+   
+	/* Initialization of matrices */
+
+    for(i=0;i<n;i++)
+      {
+       color[i]=WHITE;
+       pred[i]=NIL;
+      }
+
+    /* set discovery time of starting node as 0 */
+
+    time=0;
+
+	printf("\n Node\tFinish Time\n");
+	printf("----\t-----------\n");
+    for(i=0;i<n;i++)
+     {
+		/* If the color of node is white then call dfs */
+
+      if(color[i] == WHITE)
+      dfs(i);
+     }
+  }
+
+  int dfs(int u)
+   {
+
+    /* 1. Update the color of u as gray 
+	   2. update discovery time of node u as (current discvoery time + 1).
+	   3. For each node adjacent of u, check if the node is reachable (i.e.,
+	      Adjacent node value is not infinity and color of adjacent node is white).If so
+		  then:
+
+		  a. set u's adjacent node's predecessor as u in the predecessor matrix.
+		  b. call dfs recursively with the each value of u's adjacent node as function argument to dfs(). 
+	
+	   4. update color of u as black once step#3 is done
+	   5. update discovery time of node u as (current discvoery time + 1).
+	   6. Update u's finish time as updated discovery time of step 5
+	   7. Print the finishing time of u.
+	*/
+   }
